88a0253ddd5bbab3fda57aa646d9b9f8c33aca3a
nick <nikkitanazarow@yandex.ru>
2023-03-01 17:35:26 +0300

    1

---
+++ Asteroids-MVP-PassiveView/Assets/Sources/Presenter/ShipPresenter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ShipPresenter : Presenter
+{
+    private Root _init;
+    private int _health = 3;
+
+    public void Init(Root init)
+    {
+        _init = init;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            _health--;
+            Debug.Log($"Ваше текущее здоровье: {_health}");
+            
+            if (_health == 0)
+            {
+                _init.DisableShip();
+            }
+            
+        }
+    }
+}
